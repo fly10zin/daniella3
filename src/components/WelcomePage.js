71ccc645377d45a08f1854b1719c757d659a70e6
fly10zin <116747138+fly10zin@users.noreply.github.com>
2025-02-03 00:34:51 -0500

    Update WelcomePage.js

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -6,22 +6,22 @@ import "./Counter.css";
 function WelcomePage() {
   const imageUrls = [
     "https://www.dropbox.com/scl/fi/mw9avo1d2e9hqngmb1txt/Dani-7.jpg?rlkey=sv9d9sctsp6jflnb8mg64f3u1&st=3w7i9xt8&raw=1",
-    "https://www.dropbox.com/scl/fi/qz120g7qasli6t0bwtphe/Dani-2.jpg?rlkey=umqsdyw35nbk5py4l559hpnj9&st=d5dly62i&dl=1",
-    "https://www.dropbox.com/scl/fi/2dehwr67jcw2vfri9f3mo/Dani-3.jpg?rlkey=lrsmu021l2hl692ot1ya4azlh&st=z4r1nmcd&dl=1",
-    "https://www.dropbox.com/scl/fi/pm0zka3nb9o54oqj0uetf/Dani-4.jpg?rlkey=bobwtfve8bq7efftae46et9fu&st=7ftq03pb&dl=1",
-    "https://www.dropbox.com/scl/fi/ubzfmyo65ryouph3ekm5a/Dani-5.jpg?rlkey=z898c13g78s8gp20agif4pns6&st=pp3ot29s&dl=1",
-    "https://www.dropbox.com/scl/fi/id2rw8sz4ezk3kewv9wa1/Dani-6.jpg?rlkey=lg0jlxum5f1sd3vr0dcayotim&st=d2fvmr76&dl=1",
-    "https://www.dropbox.com/scl/fi/6y71v63rru22ilyiztqj6/Dani-8.jpg?rlkey=p2wnuwcbhcsd1z7xkogqnpe97&st=obvpylp1&dl=1",
-    "https://www.dropbox.com/scl/fi/vyiciu72j66k93glo0atn/Dani-9.jpg?rlkey=4r5l1v169dlsnmkw1yaa4yvi3&st=hx553t56&dl=1",
-    "https://www.dropbox.com/scl/fi/4tskl8qta1lppc1pksgll/Dani-10.jpg?rlkey=40czte0ca2x5bmu4msx3uip3k&st=6h7vc4y7&dl=1",
-    "https://www.dropbox.com/scl/fi/qyo6zf1zqzgkd18ugf7cp/Dani-1.jpg?rlkey=jcuaz2voposro3gptx2zctgpf&st=ncsdl23r&dl=1",
-    "https://www.dropbox.com/scl/fi/5p289xmckjw8o4ienhorv/Photo-2024-12-05-10-22-42-PM.jpg?rlkey=yyrxef323z8vq07xvyyius4ex&st=6aocb6a9&dl=1",
+    "https://www.dropbox.com/scl/fi/qz120g7qasli6t0bwtphe/Dani-2.jpg?rlkey=umqsdyw35nbk5py4l559hpnj9&st=d5dly62i&raw=1",
+    "https://www.dropbox.com/scl/fi/2dehwr67jcw2vfri9f3mo/Dani-3.jpg?rlkey=lrsmu021l2hl692ot1ya4azlh&st=z4r1nmcd&raw=1",
+    "https://www.dropbox.com/scl/fi/pm0zka3nb9o54oqj0uetf/Dani-4.jpg?rlkey=bobwtfve8bq7efftae46et9fu&st=7ftq03pb&raw=1",
+    "https://www.dropbox.com/scl/fi/ubzfmyo65ryouph3ekm5a/Dani-5.jpg?rlkey=z898c13g78s8gp20agif4pns6&st=pp3ot29s&raw=1",
+    "https://www.dropbox.com/scl/fi/id2rw8sz4ezk3kewv9wa1/Dani-6.jpg?rlkey=lg0jlxum5f1sd3vr0dcayotim&st=d2fvmr76&raw=1",
+    "https://www.dropbox.com/scl/fi/6y71v63rru22ilyiztqj6/Dani-8.jpg?rlkey=p2wnuwcbhcsd1z7xkogqnpe97&st=obvpylp1&raw=1",
+    "https://www.dropbox.com/scl/fi/vyiciu72j66k93glo0atn/Dani-9.jpg?rlkey=4r5l1v169dlsnmkw1yaa4yvi3&st=hx553t56&raw=1",
+    "https://www.dropbox.com/scl/fi/4tskl8qta1lppc1pksgll/Dani-10.jpg?rlkey=40czte0ca2x5bmu4msx3uip3k&st=6h7vc4y7&raw=1",
+    "https://www.dropbox.com/scl/fi/qyo6zf1zqzgkd18ugf7cp/Dani-1.jpg?rlkey=jcuaz2voposro3gptx2zctgpf&st=ncsdl23r&raw=1",
+    "https://www.dropbox.com/scl/fi/5p289xmckjw8o4ienhorv/Photo-2024-12-05-10-22-42-PM.jpg?rlkey=yyrxef323z8vq07xvyyius4ex&st=6aocb6a9&raw=1",
   ];
   const startDate = new Date("2025-01-18T00:00:00");
 
   const handleNotify = async () => {
     try {
-       const response = await fetch(process.env.REACT_APP_API_URL + "/notify", {
+      const response = await fetch(process.env.REACT_APP_API_URL + "/notify", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: "Daniella pressed the button!" }),
