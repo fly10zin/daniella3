aa1f2c4467e394372bfd97555d09e1a0278fa495
fly10zin <116747138+fly10zin@users.noreply.github.com>
2025-02-02 22:36:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.post("/notify", async (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
