d6633252a5f4bf9ad3fb1ac251cdf84b8dd3f7bb
fly10zin <116747138+fly10zin@users.noreply.github.com>
2025-02-02 23:12:13 -0500

    Update WelcomePage.js

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -21,7 +21,7 @@ function WelcomePage() {
 
   const handleNotify = async () => {
     try {
-      const response = await fetch("http://localhost:5000/notify", {
+      const response = await fetch(process.env.REACT_APP_API_URL + "/notify", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: "Daniella pressed the button!" }),
